9b6c14d749da884f85e5194d89c01c1263f073d5
JYE01 <160466441+JYE01@users.noreply.github.com>
2025-04-29 18:02:58 +1000

    Base Structure
    Skeleton code

---
+++ Assignment3_WhatToEat/Assignment3_WhatToEat/Assignment3_WhatToEatApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment3_WhatToEatApp.swift
+//  Assignment3_WhatToEat
+//
+//  Created by Junhui Ye on 29/4/2025.
+//
+
+import SwiftUI
+
+@main
+struct Assignment3_WhatToEatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
